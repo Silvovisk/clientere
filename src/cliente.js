f140497419bfcb6cef1036862120d816ac0aef56
Silvovisk <vascsilvia@gmail.com>
2022-12-05 22:31:54 -0300

    Melhorias na classe Cliente

--- src/cliente.js
+++ src/cliente.js
@@ -1,7 +1,24 @@
-export class Cliente { 
+export class Cliente {
+    static count = 0;
+    static listaClientes = [];
+    id;
     constructor(nome, tel, bairro){ 
-        this.nome = nome;
-        this.tel = tel;
-        this.bairro = bairro; 
+        if (!nome || !tel || !bairro){ 
+            throw new Error("Dados inválidos"); 
+        } else {
+            this.nome = nome;
+            this.tel = tel;
+            this.bairro = bairro; 
+
+            this.id = ++Cliente.count;
+
+            //push
+            this.#addClienteLista(this); 
+        }
+    }
+
+    #addClienteLista(novoCliente){
+        Cliente.listaClientes.push(novoCliente);
     }
 }
+
--- src/cliente.test.js
+++ src/cliente.test.js
@@ -1,16 +1,34 @@
 import{Cliente} from "./cliente.js"
 
 describe('Modulo do Cliente', () => {
-    test('Criação da instância do cliente', () => {
-        expect(new Cliente()).toBeInstanceOf(Cliente);
+
+    // beforeEach(() => {
+    //     jest.clearAll;
+    //   });
+
+
+    test('Não deve criar a instância do Cliente se não passar os parâmetros', () => {
+        expect(() => new Cliente()).toThrow('Dados inválidos');
     });
 
     test('Criou o Cliente com o nome, telefone e bairro', () => {
         const cliente1 = new Cliente("Cintia", "11999990000", "Centro");
+        expect(cliente1).toBeInstanceOf(Cliente);
         expect(cliente1.nome).toBe("Cintia");
         expect(cliente1.tel).toBe("11999990000");
         expect(cliente1.bairro).toBe("Centro");
     });
+
+    test('Verificando o ID dos clientes', () => {
+        const cliente1 = new Cliente("Cintia", "11999990000", "Centro");
+        const cliente2 = new Cliente("Renata", "11999991111", "Itaquera");
+        expect(cliente1.id).toBe(2);
+        expect(cliente2.id).toBe(3);
+        expect(Cliente.listaClientes.length).toBe(3);
+        console.log(Cliente);
+    });
+
+    
 });
 
 
